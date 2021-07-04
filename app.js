345730b34f3b848ab3c3c1e97a8ddc11aaa6f87c
Ahmed <darwish144.ad@gmail.com>
2021-07-04 15:29:27 +0300

    version for creating current day!

--- app.js
+++ app.js
@@ -1,29 +1,67 @@
-
-//jsjint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express()
+app.set('view engine', 'ejs');
 
 
-app.get("/", function(req,res){
+
+app.get("/", function(req, res) {
 
   var today = new Date();
   var currentDay = today.getDate();
-  if (currentDay === 6 || currentDay === 0 ) {
-    res.write("<h1>WOW it weekend !!<h1>");
-  }else {
+  var day = "";
+  //
+  // if (currentDay === 6 || currentDay === 0 ) {
+  //   day = "Weekend";
+  //
+  //
+  // }else {
+  //   day = "Weekday!";
+  // }
+  switch (currentDay) {
 
-    res.sendFile(__dirname + "/index.html");
+    case 1:
+      day = "Sato";
+      break;
+
+    case 2:
+      day = "Sun";
+      break;
+
+    case 3:
+      day = "Mon";
+      break;
+
+    case 4:
+      day = "Tue";
+      break;
+
+    case 5:
+      day = "Wed";
+      break;
+
+    case 6:
+      day = "Thus";
+      break;
+
+    case 7:
+      day = "Fri";
+      break;
+
+    default:
+    console.log("Something is Wrong: current day is" + currentDay );
 
   }
 
+  res.render("list", {
+    kindOfDay: day
+  });
 
 
 });
 
-app.listen(3000,function(){
+app.listen(3000, function() {
 
   console.log("Server running at port 3000");
 
